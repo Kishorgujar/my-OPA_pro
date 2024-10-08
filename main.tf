4e9764d2fbaf87ed2c19850cf4e3b1eef94b9627
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-09 01:42:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "automation-cicd-pro-${var.region}-${random_string.bucket_suffix.result}"
+  bucket = lower("automation-cicd-pro-ap-south-1-${random_string.bucket_suffix.result}")
 }
 
 resource "random_string" "bucket_suffix" {
