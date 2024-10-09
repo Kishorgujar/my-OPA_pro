1c62546c376583cb5aea3b4e0983e7343e6320f1
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-09 11:30:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = var.region
 }
-
+ 
 resource "aws_s3_bucket" "example" {
-  bucket = lower("automation-cicd-pro-ap-south-1-${random_string.bucket_suffix.result}")
+  # Changed the bucket name to trigger an update action
+  bucket = lower("automation-cicd-pro-ap-south-1-modified-${random_string.bucket_suffix.result}")
 }
-
+ 
 resource "random_string" "bucket_suffix" {
   length  = 8
   special = false
 }
-
